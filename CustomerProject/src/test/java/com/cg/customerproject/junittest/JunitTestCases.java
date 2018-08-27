df401741b099ac6e614e2d21f9c0a6a21c4b21c3
mehekthaker <mehekthaker@gmail.com>
2018-08-27 17:48:55 +0530

    One test case running

--- CustomerProject/src/test/java/com/cg/customerproject/junittest/JunitTestCases.java
+++ CustomerProject/src/test/java/com/cg/customerproject/junittest/JunitTestCases.java
@@ -1,7 +1,6 @@
 package com.cg.customerproject.junittest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -16,23 +15,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.cg.customerproject.App;
 import com.cg.customerproject.controller.CustomerController;
 import com.cg.customerproject.service.ServiceImpl;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = App.class)
 //@WebMvcTest(value=CustomerController.class, secure=false)
 public class JunitTestCases {
 
 //	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+    private WebApplicationContext wac;
 	
 	@Mock 
 	private ServiceImpl service;
@@ -44,8 +52,10 @@ public class JunitTestCases {
 	
 	@Before
 	public void setUp() {
-		mockMvc = MockMvcBuilders.standaloneSetup(customerController)
-				.build();
+		
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		/*mockMvc = MockMvcBuilders.standaloneSetup(customerController)
+				.build();*/
 //		System.out.println(mockMvc);
 //		System.out.println(service);
 //		System.out.println(customerController);
@@ -63,6 +73,103 @@ public class JunitTestCases {
 	}
 	
 	
+	
+	@Test
+	public void verifyGetOperationById() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/customer/119").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.customerId").exists())
+		.andExpect(jsonPath("$.customerName").exists())
+		.andExpect(jsonPath("$.contactNumber").exists())
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		.andExpect(jsonPath("$.id").value(3))
+		.andExpect(jsonPath("$.text").value("Build the artifacts"))
+		.andExpect(jsonPath("$.completed").value(false))
+		.andDo(print());
+	}
+	
+	
+	
 	@Test	
 	public void verifyGetOperation() throws Exception {
 		//String json = "{\"customerId\":\"122\",\"customerName\":\"Nyaah\",\"contactNumber\":\"986755\",\"emailId\":\"nyaah@gmail.com\",\"dateOfBirth\":\"22/4/1993\"}";
@@ -70,7 +177,7 @@ public class JunitTestCases {
 		mockMvc.perform(get("/customers")
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andDo(print())
-				.andExpect(jsonPath("$", Matchers.hasSize(8))).andDo(print());
+				.andExpect(jsonPath("$", Matchers.hasSize(9))).andDo(print());
 				/*.andExpect(jsonPath("$.customerName", Matchers.is("Nyaah")))
 				.andExpect(jsonPath("$.contactNumber", Matchers.is(986755)))
 				.andExpect(jsonPath("$.emailId", Matchers.is("nyaah@gmail.com")))
@@ -82,13 +189,27 @@ public class JunitTestCases {
 	
 	
 	@Test
-	@Ignore
+	//@Ignore
 	public void testPost() throws Exception {
-		String json = "{\"customerId\":\"122\",\"customerName\":\"Nyaah\",\"contactNumber\":\"986755\",\"emailId\":\"nyaah@gmail.com\",\"dateOfBirth\":\"22/4/1993\"}";
+		//String json = "{\"customerId\":\"122\",\"customerName\":\"Nyaah\",\"contactNumber\":\"986755\",\"emailId\":\"nyaah@gmail.com\",\"dateOfBirth\":\"22/4/1993\"}";
 		
-		mockMvc.perform(post("/customer/add")
+		/*((MockHttpServletRequestBuilder) mockMvc.perform(post("/customer/add")
 				.contentType(MediaType.APPLICATION_JSON).content(json))
-				.andExpect(status().isOk()).andDo(print());
+				.andExpect(status().isOk()).andDo(print()))*/
+				mockMvc.perform(MockMvcRequestBuilders.post("/customer/add")
+		        .contentType(MediaType.APPLICATION_JSON).content("{\"customerName\":\"Amaya\",\"contactNumber\":\"9844900\",\"emailId\":\"amaya@gmail.com\",\"dateOfBirth\":\"2/5/1990\"}")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.customerId").exists())
+				.andExpect(jsonPath("$.customerName").exists())
+				.andExpect(jsonPath("$.contactNumber").exists())
+				.andExpect(jsonPath("$.emailId").exists())
+				.andExpect(jsonPath("$.dateOfBirth").exists())
+				/*.andExpect(jsonPath("$.customerId").value("123"))*/
+				.andExpect(jsonPath("$.customerName").value("Amaya"))
+				.andExpect(jsonPath("$.contactNumber").value(9844900))
+				.andExpect(jsonPath("$.emailId").value("amaya@gmail.com"))
+				.andExpect(jsonPath("$.dateOfBirth").value("2/5/1990"))
+				.andDo(print());
 				/*.andExpect(jsonPath("$.customerId", Matchers.is(122)))
 				.andExpect(jsonPath("$.customerName", Matchers.is("Nyaah")))
 				.andExpect(jsonPath("$.contactNumber", Matchers.is(986755)))
